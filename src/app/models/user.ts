4182ea99614cfa764e5c4bd7eb1a998882ae26fa
Vola <Vola.RAMAMONJISOA@pulse.mg>
2021-01-05 10:48:02 +0300

    user model

---
+++ src/app/models/user.ts
@@ -0,0 +1,12 @@
+export class User {
+    iduser: number;
+    matricule: string;
+    firstname: string;
+    lastname: string;
+    phonenumber: string;
+    email: string;
+    login: string;
+    password: string;
+    isdeleted: boolean;
+    accessToken?: string;
+}
